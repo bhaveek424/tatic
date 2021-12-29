9148df429a43f63159cfcdab7ab1de3807fa44b5
bhaveek424 <jainbhaveek.v727@gmail.com>
2021-12-30 01:49:23 +0530

    playlist

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -35,9 +35,22 @@ const navMenu = [
         icon: MdLibraryMusic,
         route: '/library'
     }
-
-
 ]
+
+const musicMenu = [
+    {
+        name: 'Create Playlist',
+        icon: MdPlaylistAdd,
+        route: '/'
+    },
+    {
+        name: 'Favorites',
+        icon: MdFavorite,
+        route: '/'
+    }
+]
+
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i+1}`)
 
 const Sidebar = () => {
     return (
@@ -48,7 +61,7 @@ const Sidebar = () => {
             paddingX="5px" 
             color="gray"
             >
-                <Box paddingY="20px">
+                <Box paddingY="20px" height="100%">
                     <Box width="120px" marginBottom="20px" paddingX="20px">
                         <NextImage src="/logo.svg" height={60} width={120} />
                     </Box>
@@ -67,6 +80,48 @@ const Sidebar = () => {
                                 </ListItem>
                             ))}
                         </List>
+                    </Box>
+                    <Box marginTop="20px ">
+                        <List spacing={2}>
+                            {musicMenu.map(menu => (
+                                <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                                     <LinkBox>
+                                        <NextLink href={menu.route} passHref>
+                                            <LinkOverlay>
+                                                <ListIcon as={menu.icon} color="white" marginRight="20px" />
+                                                {menu.name}
+                                            </LinkOverlay>
+                                        </NextLink>
+                                    </LinkBox>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Box>
+                    <Divider color="gray.800" />
+                    <Box height="66%" overflowY="auto" paddingY="20px" 
+                    sx={{
+                        '&::-webkit-scrollbar': {
+                            width: '12px',
+                          },
+                          '&::-webkit-scrollbar-track': {
+                            width: '6px',
+                          },
+                          '&::-webkit-scrollbar-thumb': {
+                            background: '#92cdaf',
+                            borderRadius: '24px',
+                          },
+                    }}>
+                                <List spacing={2}>
+                                    {playlists.map(playlist => (
+                                        <ListItem paddingX="20px" key={playlist} >
+                                            <LinkBox>
+                                                <NextLink href="/">
+                                                    <LinkOverlay> {playlist} </LinkOverlay>
+                                                </NextLink>
+                                            </LinkBox>
+                                        </ListItem>
+                                    ))}
+                                </List>
                     </Box>
                 </Box>
             </Box>
