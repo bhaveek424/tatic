9e01505e384a4ea5edaf126d0994cc3ff3fe3c3f
bhaveek424 <jainbhaveek.v727@gmail.com>
2021-12-29 20:21:36 +0530

    'layout'

---
+++ pages/_app.tsx
@@ -0,0 +1,43 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import 'reset-css';
+import PlayerLayout from "../components/playerLayout";
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#F5f5f5',
+      200: '#EEEEEE',
+      300: '#e0e0e0',
+      400: '#BDBDBD',
+      500: '#9E9E9E',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            outline: "none",
+            boxShadow: "none",
+          }
+        }
+      }
+    }
+  }
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
