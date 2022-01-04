5c4f8a03c444aeea97fb455f7568649c1f5da350
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-01-04 19:45:32 +0530

    song state

--- components/playerBar.tsx
+++ components/playerBar.tsx
@@ -1,18 +1,27 @@
 import { Box, Flex, Text } from "@chakra-ui/layout";
 import Player from "./player";
+import { useStoreState } from "easy-peasy";
 
 const PlayerBar = () => {
+  const songs = useStoreState((state: any) => state.activeSongs);
+  const activeSong = useStoreState((state: any) => state.activeSong);
+
   return (
     <Box height="100px" width="100vw" bg="gray.900" padding="10px">
       <Flex align="center">
-        {/* Left side of player bar */}
-        <Box padding="20px" color="white" width="30%">
-          <Text fontSize="large">Song Name</Text>
-          <Text fontSize="sm">Artist name</Text>
-        </Box>
-        {/* middle of player bar */}
+        {activeSong ? (
+          // Left side of player bar
+          <Box padding="20px" color="white" width="30%">
+            <Text fontSize="large">{activeSong.name}</Text>
+            <Text fontSize="small">{activeSong.artist.name}</Text>
+          </Box>
+        ) : null}
+
         <Box width="40%">
-          <Player />
+          {activeSong ? (
+            // middle of player bar
+            <Player songs={songs} activeSong={activeSong} />
+          ) : null}
         </Box>
       </Flex>
     </Box>
--- components/songsTable.tsx
+++ components/songsTable.tsx
@@ -3,8 +3,16 @@ import { Table, Thead, Td, Tr, Tbody, Th, IconButton } from "@chakra-ui/react";
 import { BsFillPlayFill } from "react-icons/bs";
 import { AiOutlineClockCircle } from "react-icons/ai";
 import { formatDate, formatTime } from "../lib/formatters";
+import { useStoreActions } from "easy-peasy";
 
 const SongTable = ({ songs }) => {
+  const playSongs = useStoreActions((store: any) => store.changeActiveSongs);
+  const setActiveSong = useStoreActions((store: any) => store.changeActiveSong);
+
+  const handlePlay = (activeSong?) => {
+    setActiveSong(activeSong || songs[0]);
+    playSongs(songs);
+  };
   return (
     <Box bg="transparent" color="white">
       <Box padding="10px" marginBottom="20px">
@@ -15,6 +23,7 @@ const SongTable = ({ songs }) => {
             colorScheme="green"
             size="lg"
             isRound
+            onClick={() => handlePlay()}
           />
         </Box>
         <Table variant="unstyled">
@@ -38,7 +47,8 @@ const SongTable = ({ songs }) => {
                   },
                 }}
                 key={song.id}
-                cursor="cursor"
+                cursor="pointer"
+                onClick={() => handlePlay(song)}
               >
                 <Td>{i + 1}</Td>
                 <Td>{song.name}</Td>
