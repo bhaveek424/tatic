4c9352758d4571bc0e8dc9f360a251038580063e
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-01-04 15:13:29 +0530

    'playerbar-controls-ui'

--- components/player.tsx
+++ components/player.tsx
@@ -46,14 +46,6 @@ const Player = () => {
           <IconButton
             outline="none"
             variant="link"
-            aria-label="pause"
-            fontSize="40px"
-            color="white"
-            icon={<MdOutlinePauseCircleFilled />}
-          />
-          <IconButton
-            outline="none"
-            variant="link"
             aria-label="play"
             fontSize="40px"
             color="white"
@@ -62,19 +54,52 @@ const Player = () => {
           <IconButton
             outline="none"
             variant="link"
-            aria-label="shuffle"
+            aria-label="pause"
+            fontSize="40px"
+            color="white"
+            icon={<MdOutlinePauseCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="skip"
             fontSize="24px"
             icon={<MdSkipNext />}
           />
           <IconButton
             outline="none"
             variant="link"
-            aria-label="shuffle"
+            aria-label="repeat"
             fontSize="24px"
             icon={<MdOutlineRepeat />}
           />
         </ButtonGroup>
       </Center>
+
+      <Box color="gray.600">
+        <Flex justify="center" align="center">
+          <Box width="10%">
+            <Text fontSize="x-small">1:21</Text>
+          </Box>
+          <Box width="80%">
+            <RangeSlider
+              aria-label={["min", "max"]}
+              step={0.1}
+              min={0}
+              max={321}
+              id="player-range"
+            >
+              <RangeSliderTrack bg="gray.800">
+                <RangeSliderFilledTrack bg="gray.600" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          <Box width="10%" textAlign="right">
+            <Text fontSize="x-small">3:21</Text>
+          </Box>
+        </Flex>
+      </Box>
     </Box>
   );
 };
